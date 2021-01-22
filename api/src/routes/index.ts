ddbec18c0130798e0cb82ebfdca12daa464826f7
tom-p-uk <tom.p.uk1987@gmail.com>
2021-01-22 11:26:51 +0000

    Clean up Github workflows, add health check route to API

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import {Router, Request, Response} from 'express';
 import slotsRoutes from './slots';
 
 const router = Router();
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/slots', slotsRoutes.get);
 router.patch('/slots', slotsRoutes.patch);
 router.patch('/slots/:id', slotsRoutes.patchId);
+router.get('/health', (req: Request, res: Response) => res.status(200).send(true));
 
 export default router;
